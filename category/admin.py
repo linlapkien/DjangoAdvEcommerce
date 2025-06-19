da4c161abe2823d037447a61c68cd965f118b01e
Lin Lập Kiến <linlapkien@gmail.com>
2025-06-19 23:10:52 +0700

    Day 2 - Learning

--- category/admin.py
+++ category/admin.py
@@ -3,4 +3,12 @@ from .models import Category
 
 # Register your models here.
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'slug', 'description', 'image')
+    prepopulated_fields = {'slug': ('category_name',)}
+    search_fields = ('category_name',)
+    list_filter = ('category_name',)
+    ordering = ('category_name',)
+
+admin.site.register(Category, CategoryAdmin)
+
